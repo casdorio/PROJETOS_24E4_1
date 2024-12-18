685a582ec38a230e87555b1618da506040243812
casdorio <casdorio@gmail.com>
2024-12-18 17:08:06 -0300

    09

--- AT/q09.py
+++ AT/q09.py
@@ -1,4 +1,4 @@
-class HashtableSimples:
+class Hashtable:
     def __init__(self, tamanho=10):
         self.tamanho = tamanho
         self.tabela = [None] * tamanho
@@ -17,13 +17,11 @@ class HashtableSimples:
 perfil1 = {"nome_usuario": "casdorio", "nome_completo": "Carlos Adriano", "email": "casdorio@gmail.com"}
 perfil2 = {"nome_usuario": "casdorio2", "nome_completo": "Carlos Adriano 2", "email": "casdorio2@gmail.com"}
 
-hashtable = HashtableSimples()
+hashtable = Hashtable()
 
-# Armazenando perfis
 hashtable.armazenar("casdorio", perfil1)
 hashtable.armazenar("casdorio2", perfil2)
 
-# Recuperando perfis
 perfil_recuperado = hashtable.recuperar("casdorio")
 if perfil_recuperado:
     print(f"Nome completo: {perfil_recuperado['nome_completo']}, Email: {perfil_recuperado['email']}")
